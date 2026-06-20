b268771ae8b5685c82c638184c8670a99fd6ebdb
daemunozr <dae.munozr@duocuc.cl>
2026-06-20 15:15:55 -0400

    añadido test

--- src/test/java/com/sivebo/eureka_server/EurekaServerApplicationTests.java
+++ src/test/java/com/sivebo/eureka_server/EurekaServerApplicationTests.java
@@ -1,13 +1,41 @@
 package com.sivebo.eureka_server;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class EurekaServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@LocalServerPort
+	int port;
 
+	private final RestTemplate restTemplate = new RestTemplate();
+
+	@Test
+	void contextLoads() {}
+
+	@Test
+	void eurekaRegistryEndpoint_returns200() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+		ResponseEntity<String> response = restTemplate.exchange(
+			"http://localhost:" + port + "/eureka/apps",
+			HttpMethod.GET,
+			new HttpEntity<>(headers),
+			String.class
+		);
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertNotNull(response.getBody());
+	}
 }
